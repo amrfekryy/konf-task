2b452909cd4268fd5040a097a1b9e89def0a3d21
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2020-10-12 12:41:42 +0200

    add initial graphql schema

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+const { ApolloServer } = require('apollo-server');
+
+const typeDefs = require('./schema');
+// const resolvers = require('./resolvers');
+// const { createStore } = require('./utils');
+
+// const GeneralAPI = require('./datasources/general');
+
+// const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  // resolvers,
+  // dataSources: () => ({
+  //   generalAPI: new GeneralAPI({ store }),
+  //   userAPI: new UserAPI({ store }), 
+  // }),
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
---
+++ server/src/schema.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+
+  type Item {
+    id: ID!
+    name: String!
+    type: String!
+    price: Float!
+    photo: String
+  }
+
+  type Query {
+    getItems: [Item]!
+    getItem: Item!
+  }
+  
+  type Mutation {
+    
+    addItem(    
+      name: String!
+      type: String!
+      price: Float!
+      photo: String
+    ): Response!
+    
+    updateItem(
+      id: ID!,
+      name: String!
+      type: String!
+      price: Float!
+      photo: String 
+    ): Response!
+
+    deleteItem(id: ID!): Response!
+
+  }
+  
+  type Response {
+    success: Boolean!
+    resMessage: String
+    items: [Item]
+    item: Item
+  }
+`;
+
+module.exports = typeDefs;
