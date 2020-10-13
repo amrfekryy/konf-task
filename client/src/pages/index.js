871db07c7e4b765ad732ec74cabbc758d0ce02c4
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2020-10-13 11:03:17 +0200

    refactor

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,13 +1,25 @@
 import React, { Fragment } from 'react';
 import { Router } from '@reach/router';
-import { Home, Form, NotFound } from './pages'
+// import { Home, Form, NotFound } from './pages'
+import Page from './page'
+import Menu from 'components/menu'
+import Form from 'components/form'
+import Travolta from 'assets/travolta.gif'
+
+const NotFound = () => <img 
+    src={Travolta} alt="Nothing Here!" 
+    style={{ display:'block', margin:'auto'}}
+  /> 
+
 
 export default function Pages() {
   return (
-    <Router>
-      <Home      path="/" />
-      <Form      path="/form" />                 
-      <NotFound  default />
-    </Router>
+    <Page>
+      <Router style={{width: '100%'}}>
+        <Menu      path="/" />
+        <Form      path="/form" />                 
+        <NotFound  default />
+      </Router>
+    </Page>
   );
 }
